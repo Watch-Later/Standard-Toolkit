aa4a0ba988eabcab02f458af1071d1fec35cf114
Peter Wagner <peterwagner@live.co.uk>
2021-02-08 09:08:39 +0000

    * Files

---
+++ Source/Krypton Components/Visual Studio Templates/C#/KryptonFormCS/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Krypton.Toolkit;
+using System.Windows.Forms;
+
+namespace KryptonFormTemplateCS
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
